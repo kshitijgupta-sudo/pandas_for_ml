0f7044489e69e42f0211d352f6eea90a69d120f5
Kshitij Gupta <kshitijgupta209@gmail.com>
2026-07-04 07:01:13 +0530

    The first commit

---
+++ pandas_prac1.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+
+# Create a sample DataFrame
+
+df = pd.DataFrame({
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],
+    'Age': [25, 30, 35, 40, 45],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+})  
+
+print(df)
